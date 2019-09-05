da15e5fa10731421951bf7b0068e3848e4844d17
Austin Wise <austin@wisebydesign.io>
2019-09-05 12:35:20 -0500

    update package.json for troubleshooting deployment

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import Landing from './components/LandingGroup/landing';
 import UserArea from './components/Main/userArea';
 
-function App() {
-  const loggedIn = true;
-  return (
-    <div className="App" id="#override">
-      <Router>
-        <Switch>
-          {!loggedIn && <Route path="/" component={Landing} />}
-          {loggedIn && <Route path="/" component={UserArea} />}
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const loggedIn = true;
+    return (
+      <div className="App" id="#override">
+        <Router>
+          <Switch>
+            {!loggedIn && <Route path="/" component={Landing} />}
+            {loggedIn && <Route path="/" component={UserArea} />}
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
