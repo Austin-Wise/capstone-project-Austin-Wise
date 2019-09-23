90dd9e2436954885d470650c8539a84a6681ce79
Austin Wise <austin@wisebydesign.io>
2019-09-23 13:34:32 -0400

    Update Server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,6 +2,7 @@
 const log = require('debug')('api:logging');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = require('./app');
 // set the port to either the one passed from the environment variables or 5000
 const port = process.env.PORT || 5000;
@@ -17,13 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up assets
   // like main.js and main.css
-  app.use(express.static('reactjs/build'));
+  app.use(express.static('../reactjs/build'));
   // Express will serve up the index.html
   // if it doesn't recognize the route
-  const path = require('path');
-
   app.get('*', (req, res) => {
-    res.sendfile(path.resolve(__dirname, 'reactjs', 'build', 'index.html'));
+    res.sendfile(path.resolve(__dirname, '../reactjs', 'build', 'index.html'));
   });
 }
 
